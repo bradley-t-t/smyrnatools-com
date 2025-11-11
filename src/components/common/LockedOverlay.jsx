4fbfaaf9207f28df9945a8fd4a467a1bc8d025d3
Trenton Taylor <trenton.taylor.email@gmail.com>
2025-11-11 13:43:45 -0600

    3.0.3.1 Update
    - Removed plant check from LockedOverlay.jsx.

--- src/components/common/LockedOverlay.jsx
+++ src/components/common/LockedOverlay.jsx
@@ -54,18 +54,12 @@ function LockedOverlay({reason}) {
         if (reasonToUse === 'invalid-session') {
             return 'Your session has expired or is invalid. Please sign out and log in again to continue.';
         }
-        if (reasonToUse === 'no-plant') {
-            return 'Your plant has not been assigned. Please contact your district manager to assign a plant to your profile.';
-        }
         return 'You must contact your district manager for them to approve your sign-up.';
     };
 
     const getTitle = () => {
         if (reasonToUse === 'invalid-session') {
             return 'Session Invalid';
-        }
-        if (reasonToUse === 'no-plant') {
-            return 'Plant Not Assigned';
         }
         return 'Access Pending';
     };
